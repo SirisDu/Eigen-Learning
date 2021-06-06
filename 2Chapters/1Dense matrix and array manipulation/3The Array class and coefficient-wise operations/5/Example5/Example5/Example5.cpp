2146e4b6644d0f69c66b1f3f57fe1ad8826298ef
Siris Du <dujr0017@163.com>
2021-06-06 12:54:08 +0800

    Converting between array and matrix expressions

---
+++ 2Chapters/1Dense matrix and array manipulation/3The Array class and coefficient-wise operations/5/Example5/Example5/Example5.cpp
@@ -0,0 +1,42 @@
+﻿// Example5.cpp
+// Converting between array and matrix expressions
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+    Eigen::MatrixXf m(2, 2);
+    Eigen::MatrixXf n(2, 2);
+    Eigen::MatrixXf result(2, 2);
+
+    m << 1, 2,
+         3, 4;
+    n << 5, 6,
+         7, 8;
+
+    result = m * n;
+    std::cout << "-- Matrix m*n: --" << std::endl
+        << result << std::endl << std::endl;
+
+    result = m.array() * n.array();
+    std::cout << "-- Array m*n: --" << std::endl
+        << result << std::endl << std::endl;
+
+    result = m.cwiseProduct(n);
+    std::cout << "-- With cwiseProduct: --" << std::endl
+        << result << std::endl << std::endl;
+
+    result = m.array() + 4;
+    std::cout << "-- Array m + 4: --" << std::endl
+        << result << std::endl << std::endl;
+
+
+    result = (m.array() + 4).matrix() * m;
+    std::cout << "-- Combination 1: --" << std::endl
+        << result << std::endl << std::endl;
+
+    result = (m.array() * n.array()).matrix() * m;
+    std::cout << "-- Combination 2: --" << std::endl
+        << result << std::endl << std::endl;
+}
