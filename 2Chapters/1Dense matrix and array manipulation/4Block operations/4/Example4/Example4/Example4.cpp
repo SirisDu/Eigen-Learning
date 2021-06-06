c27dcd189e3dcd6b4c22bae1d5c9817b62c80c01
Siris Du <dujr0017@163.com>
2021-06-06 13:21:35 +0800

    Block operations for vectors

---
+++ 2Chapters/1Dense matrix and array manipulation/4Block operations/4/Example4/Example4/Example4.cpp
@@ -0,0 +1,18 @@
+﻿// Example4.cpp
+// Block operations for vectors
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+    Eigen::ArrayXf v(6);
+    v << 1, 2, 3, 4, 5, 6;
+    std::cout << "v.head(3) =" << std::endl
+        << v.head(3) << std::endl << std::endl;
+    std::cout << "v.tail<3>() = " << std::endl
+        << v.tail<3>() << std::endl << std::endl;
+    v.segment(1, 4) *= 2;
+    std::cout << "after 'v.segment(1,4) *= 2', v =" << std::endl
+        << v << std::endl;
+}
