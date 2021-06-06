ec8f9e951595e6bfc9aa8c78642df6b3e4d4d8ac
Siris Du <dujr0017@163.com>
2021-06-06 13:14:46 +0800

    Columns and rows

---
+++ 2Chapters/1Dense matrix and array manipulation/4Block operations/2/Example2/Example2/Example2.cpp
@@ -0,0 +1,19 @@
+﻿// Example2.cpp
+// Columns and rows
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+    Eigen::MatrixXf m(3, 3);
+    m << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+    std::cout << "Here is the matrix m:" << std::endl << m << std::endl;
+    std::cout << "2nd Row: " << m.row(1) << std::endl;
+    m.col(2) += 3 * m.col(0);
+    std::cout << "After adding 3 times the first column"
+        " into the third column, the matrix m is:\n";
+    std::cout << m << std::endl;
+}
