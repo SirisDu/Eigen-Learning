d23c8fbf81c67ba0de2e1074f9cf1a407915f209
Siris Du <dujr0017@163.com>
2021-06-06 12:34:19 +0800

    Addition and subtraction

---
+++ 2Chapters/1Dense matrix and array manipulation/3The Array class and coefficient-wise operations/2/Example2/Example2/Example2.cpp
@@ -0,0 +1,23 @@
+﻿// Example2.cpp
+// Addition and subtraction
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+    Eigen::ArrayXXf a(3, 3);
+    Eigen::ArrayXXf b(3, 3);
+    a << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+    b << 1, 2, 3,
+         1, 2, 3,
+         1, 2, 3;
+
+    // Adding two arrays
+    std::cout << "a + b = " << std::endl << a + b << std::endl << std::endl;
+
+    // Subtracting a scalar from an array
+    std::cout << "a - 2 = " << std::endl << a - 2 << std::endl;
+}
