95325293b806a048c0a94b5ec9033b399b1d9c69
Siris Du <dujr0017@163.com>
2021-06-06 13:47:19 +0800

    Usage as temporary objects

---
+++ 2Chapters/1Dense matrix and array manipulation/5Advanced initialization/3/Example3/Example3/Example3.cpp
@@ -0,0 +1,13 @@
+﻿// Example3.cpp
+// Usage as temporary objects
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+    Eigen::MatrixXf mat = Eigen::MatrixXf::Random(2, 3);
+    std::cout << mat << std::endl << std::endl;
+    mat = (Eigen::MatrixXf(2, 2) << 0, 1, 1, 0).finished() * mat;
+    std::cout << mat << std::endl;
+}
