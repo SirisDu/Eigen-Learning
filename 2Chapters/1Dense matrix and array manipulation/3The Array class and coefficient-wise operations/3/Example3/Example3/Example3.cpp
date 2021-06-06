a1eaa408cf28ce805f39ea0ef5f11cffe1b0d6ee
Siris Du <dujr0017@163.com>
2021-06-06 12:37:19 +0800

    Array multiplication

---
+++ 2Chapters/1Dense matrix and array manipulation/3The Array class and coefficient-wise operations/3/Example3/Example3/Example3.cpp
@@ -0,0 +1,16 @@
+﻿// Example3.cpp
+// Array multiplication
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+    Eigen::ArrayXXf a(2, 2);
+    Eigen::ArrayXXf b(2, 2);
+    a << 1, 2,
+         3, 4;
+    b << 5, 6,
+         7, 8;
+    std::cout << "a * b = " << std::endl << a * b << std::endl;
+}
