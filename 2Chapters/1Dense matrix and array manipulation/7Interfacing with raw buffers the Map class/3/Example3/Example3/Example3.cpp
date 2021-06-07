5bca878a6069bf68bf29d7bf0776c7adb58adc67
Siris Du <dujr0017@163.com>
2021-06-07 14:09:57 +0800

    Changing the mapped array

---
+++ 2Chapters/1Dense matrix and array manipulation/7Interfacing with raw buffers the Map class/3/Example3/Example3/Example3.cpp
@@ -0,0 +1,17 @@
+﻿// Example3.cpp
+// Changing the mapped array
+
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+
+int main()
+{
+    int data[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    Map<RowVectorXi> v(data, 4);
+    cout << "The mapped vector v is: " << v << "\n";
+    new (&v) Map<RowVectorXi>(data + 4, 5);
+    cout << "Now v is: " << v << "\n";
+}
