2eab161ddf7822733069b36cb255b67b6a1e615a
Siris Du <dujr0017@163.com>
2021-06-07 13:56:36 +0800

    Map types and declaring Map variables

---
+++ 2Chapters/1Dense matrix and array manipulation/7Interfacing with raw buffers the Map class/1/Example1/Example1/Example1.cpp
@@ -0,0 +1,20 @@
+﻿// Example1.cpp
+// Map types and declaring Map variables
+
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+int main()
+{
+    int array[8];
+    for (int i = 0; i < 8; ++i) array[i] = i;
+    std::cout << "Column-major:\n"
+        << Map<Matrix<int, 2, 4> >(array) << std::endl;
+    std::cout << "Row-major:\n"
+        << Map<Matrix<int, 2, 4, RowMajor> >(array) << std::endl;
+    std::cout << "Row-major using stride:\n"
+        << Map<Matrix<int, 2, 4>, Unaligned, Stride<1, 4> >(array)
+        << std::endl;
+}
