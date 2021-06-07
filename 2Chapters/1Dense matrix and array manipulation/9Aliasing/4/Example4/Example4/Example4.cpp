287caa5d37aa5817b54ea377284062a2a951bd56
Siris Du <dujr0017@163.com>
2021-06-07 21:07:19 +0800

    Aliasing and matrix multiplication

---
+++ 2Chapters/1Dense matrix and array manipulation/9Aliasing/4/Example4/Example4/Example4.cpp
@@ -0,0 +1,42 @@
+﻿// Example4.cpp
+// Aliasing and matrix multiplication
+
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+
+int main()
+{
+    MatrixXf matA(2, 2);
+    matA << 2, 0,
+            0, 2;
+    matA = matA * matA;
+    cout << matA << endl << endl;
+
+    MatrixXf matB(2, 2);
+    matA << 2, 0,
+            0, 2;
+    // Simple but not quite as efficient
+    matB = matA * matA;
+    cout << matB << endl << endl;
+
+    // More complicated but also more efficient
+    matB.noalias() = matA * matA;
+    cout << matB << endl << endl;
+
+    MatrixXf A1(2, 2), B1(3, 2);
+    B1 << 2, 0,
+          0, 3,
+          1, 1;
+    A1 << 2,  0,
+          0, -2;
+    A1 = (B1 * A1).cwiseAbs();
+    cout << A1 << endl << endl;
+
+    A1 << 2,  0,
+          0, -2;
+    A1 = (B1 * A1).eval().cwiseAbs();
+    cout << A1 << endl;
+}
