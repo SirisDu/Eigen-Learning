5e0c05c89b45db1198434d4dafe740d01fd5f655
Siris Du <dujr0017@163.com>
2021-06-07 20:50:17 +0800

    Resolving aliasing issues

---
+++ 2Chapters/1Dense matrix and array manipulation/9Aliasing/2/Example2/Example2/Example2.cpp
@@ -0,0 +1,27 @@
+﻿// Example2.cpp
+// Resolving aliasing issues
+
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+
+int main()
+{
+    MatrixXi mat(3, 3);
+    mat << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+    cout << "Here is the matrix mat:\n" << mat << endl;
+
+    // The eval() solves the aliasing problem
+    mat.bottomRightCorner(2, 2) = mat.topLeftCorner(2, 2).eval();
+    cout << "After the assignment, mat = \n" << mat << endl;
+
+    MatrixXf a(2, 3);
+    a << 1, 2, 3,
+         4, 5, 6;
+    cout << "Here is the initial matrix a:\n" << a << endl;
+
+    a.transposeInPlace();
+    cout << "and after being transposed:\n" << a << endl;
+}
