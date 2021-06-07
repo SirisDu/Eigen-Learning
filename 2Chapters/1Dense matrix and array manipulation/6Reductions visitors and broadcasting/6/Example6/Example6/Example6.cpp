0f3f979d4ff4c2288ae5c2f1ccbaf1642a346262
Siris Du <dujr0017@163.com>
2021-06-07 13:30:30 +0800

    Combining partial reductions with other operations

---
+++ 2Chapters/1Dense matrix and array manipulation/6Reductions visitors and broadcasting/6/Example6/Example6/Example6.cpp
@@ -0,0 +1,21 @@
+﻿// Example6.cpp
+// Combining partial reductions with other operations
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+    Eigen::MatrixXf mat(2, 4);
+    mat << 1, 2, 6, 9,
+           3, 1, 7, 2;
+
+    Eigen::MatrixXf::Index maxIndex;
+    float maxNorm = mat.colwise().sum().maxCoeff(&maxIndex);
+
+    std::cout << "Maximum sum at position " << maxIndex << std::endl;
+
+    std::cout << "The corresponding vector is: " << std::endl;
+    std::cout << mat.col(maxIndex) << std::endl;
+    std::cout << "And its sum is is: " << maxNorm << std::endl;
+}
