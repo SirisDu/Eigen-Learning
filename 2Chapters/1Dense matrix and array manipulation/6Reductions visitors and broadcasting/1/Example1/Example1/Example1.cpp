441dc1536c0a66fc29fe4413a74f46deda10c645
Siris Du <dujr0017@163.com>
2021-06-07 10:59:54 +0800

    Reductions

---
+++ 2Chapters/1Dense matrix and array manipulation/6Reductions visitors and broadcasting/1/Example1/Example1/Example1.cpp
@@ -0,0 +1,18 @@
+﻿// Example1.cpp
+// Reductions
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+    Eigen::Matrix2d mat;
+    mat << 1, 2,
+           3, 4;
+    std::cout << "Here is mat.sum():       " << mat.sum() << std::endl;
+    std::cout << "Here is mat.prod():      " << mat.prod() << std::endl;
+    std::cout << "Here is mat.mean():      " << mat.mean() << std::endl;
+    std::cout << "Here is mat.minCoeff():  " << mat.minCoeff() << std::endl;
+    std::cout << "Here is mat.maxCoeff():  " << mat.maxCoeff() << std::endl;
+    std::cout << "Here is mat.trace():     " << mat.trace() << std::endl;
+}
