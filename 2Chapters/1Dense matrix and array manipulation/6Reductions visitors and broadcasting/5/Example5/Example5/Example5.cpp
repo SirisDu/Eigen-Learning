82d455277fd222e98493858860bc7c9bd58fb297
Siris Du <dujr0017@163.com>
2021-06-07 13:26:05 +0800

    Partial reductions

---
+++ 2Chapters/1Dense matrix and array manipulation/6Reductions visitors and broadcasting/5/Example5/Example5/Example5.cpp
@@ -0,0 +1,17 @@
+﻿// Example5.cpp
+// Partial reductions
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+    Eigen::MatrixXf mat(2, 4);
+    mat << 1, 2, 6, 9,
+           3, 1, 7, 2;
+
+    std::cout << "Column's maximum: " << std::endl
+        << mat.colwise().maxCoeff() << std::endl;
+    std::cout << "Row's maximum: " << std::endl
+        << mat.rowwise().maxCoeff() << std::endl;
+}
