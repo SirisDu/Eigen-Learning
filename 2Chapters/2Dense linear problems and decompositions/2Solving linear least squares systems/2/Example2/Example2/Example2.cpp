a1e109e55b6b38313fdf17bc7f62714e23347093
Siris Du <dujr0017@163.com>
2021-06-08 10:28:01 +0800

    Using the QR decomposition

---
+++ 2Chapters/2Dense linear problems and decompositions/2Solving linear least squares systems/2/Example2/Example2/Example2.cpp
@@ -0,0 +1,16 @@
+﻿// Example2.cpp
+// Using the QR decomposition
+
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+    MatrixXf A = MatrixXf::Random(3, 2);
+    VectorXf b = VectorXf::Random(3);
+    cout << "The solution using the QR decomposition is:\n"
+        << A.colPivHouseholderQr().solve(b) << endl;
+}
