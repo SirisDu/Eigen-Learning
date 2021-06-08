f1dc46b9a38a7dd84cbea37c253918034f159d85
Siris Du <dujr0017@163.com>
2021-06-08 09:49:39 +0800

    Computing eigenvalues and eigenvectors

---
+++ 2Chapters/2Dense linear problems and decompositions/1Linear algebra and decompositions/3/Example3/Example3/Example3.cpp
@@ -0,0 +1,23 @@
+﻿// Example3.cpp
+// Computing eigenvalues and eigenvectors
+
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+
+int main()
+{
+    Matrix2f A;
+    A << 1, 2,
+         2, 3;
+    cout << "Here is the matrix A:\n" << A << endl;
+    SelfAdjointEigenSolver<Matrix2f> eigensolver(A);
+    if (eigensolver.info() != Success)
+        abort();
+    cout << "The eigenvalues of A are:\n" << eigensolver.eigenvalues() << endl;
+    cout << "Here's a matrix whose columns are eigenvectors of A \n"
+        << "corresponding to these eigenvalues:\n"
+        << eigensolver.eigenvectors() << endl;
+}
