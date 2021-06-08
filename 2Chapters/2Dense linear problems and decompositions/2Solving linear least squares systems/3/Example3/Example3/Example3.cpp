8bacd1e7fe6a8c72642bb9a63f9ccf001e782af9
Siris Du <dujr0017@163.com>
2021-06-08 10:30:18 +0800

    Using normal equations

---
+++ 2Chapters/2Dense linear problems and decompositions/2Solving linear least squares systems/3/Example3/Example3/Example3.cpp
@@ -0,0 +1,16 @@
+﻿// Example3.cpp
+// Using normal equations
+
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+    MatrixXf A = MatrixXf::Random(3, 2);
+    VectorXf b = VectorXf::Random(3);
+    cout << "The solution using normal equations is:\n"
+        << (A.transpose() * A).ldlt().solve(A.transpose() * b) << endl;
+}
