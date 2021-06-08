897f3b3f45916ca9727aa785e643a84c295623b3
Siris Du <dujr0017@163.com>
2021-06-08 10:23:47 +0800

    Using the SVD decomposition

---
+++ 2Chapters/2Dense linear problems and decompositions/2Solving linear least squares systems/1/Example1/Example1/Example1.cpp
@@ -0,0 +1,18 @@
+﻿// Example1.cpp
+// Using the SVD decomposition
+
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+    MatrixXf A = MatrixXf::Random(3, 2);
+    cout << "Here is the matrix A:\n" << A << endl;
+    VectorXf b = VectorXf::Random(3);
+    cout << "Here is the right hand side b:\n" << b << endl;
+    cout << "The least-squares solution is:\n"
+        << A.bdcSvd(ComputeThinU | ComputeThinV).solve(b) << endl;
+}
