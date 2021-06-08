f97c1ccd297896daf7ee8e0aca90a0f65eb210bf
Siris Du <dujr0017@163.com>
2021-06-08 10:16:01 +0800

    Rank-revealing decompositions

---
+++ 2Chapters/2Dense linear problems and decompositions/1Linear algebra and decompositions/7/Example7/Example7/Example7.cpp
@@ -0,0 +1,34 @@
+﻿// Example7.cpp
+// Rank-revealing decompositions
+
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+    Matrix3f A;
+    A << 1, 2, 5,
+         2, 1, 4,
+         3, 0, 3;
+    cout << "Here is the matrix A:\n" << A << endl;
+    FullPivLU<Matrix3f> lu_decomp(A);
+    cout << "The rank of A is " << lu_decomp.rank() << endl;
+    cout << "Here is a matrix whose columns form"
+        "a basis of the null-space of A:\n"
+        << lu_decomp.kernel() << endl;
+    cout << "Here is a matrix whose columns form"
+        "a basis of the column-space of A:\n"
+        << lu_decomp.image(A) << endl; // yes, have to pass the original A
+
+    Matrix2d A1;
+    A1 << 2, 1,
+          2, 0.9999999999;
+    FullPivLU<Matrix2d> lu(A1);
+    cout << "By default, the rank of A is found to be " << lu.rank() << endl;
+    lu.setThreshold(1e-5);
+    cout << "With threshold 1e-5, the rank of A is found to be "
+        << lu.rank() << endl;
+}
